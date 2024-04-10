91642167b7fc73597c146f0cf11d25f9f8110f5c
amora7741 <mora7741@gmail.com>
2024-04-10 13:43:29 -0700

    Store month as number

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  birthMonth: { type: String, required: true },
+  birthMonth: { type: Number, required: true },
   birthDay: { type: Number, required: true },
   birthYear: { type: Number, required: true },
   username: { type: String, required: true, unique: true },
